6c7fa38bca1faaa62bcc3c84aeaed15d5f8ae91b
Abe Winter <abe.winter@viam.com>
2025-02-18 14:41:38 -0500

    try adding x264 to streamConfig, mediadevices x264 cannot build

--- web/server/entrypoint_windows.go
+++ web/server/entrypoint_windows.go
@@ -4,9 +4,10 @@ package server
 
 import (
 	"go.viam.com/rdk/gostream"
+	"go.viam.com/rdk/gostream/codec/x264"
 )
 
 func makeStreamConfig() gostream.StreamConfig {
 	// TODO(RSDK-1771): support video on windows
-	return gostream.StreamConfig{}
+	return gostream.StreamConfig{VideoEncoderFactory: x264.NewEncoderFactory()}
 }
